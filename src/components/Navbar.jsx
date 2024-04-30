91fa84db7d45cf0a3ce0f5e598b00aaf9cc60d49
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-04-29 21:32:36 -0400

    Refactor CSS styles and add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar-main">
+      <div className="navmain">
+        <div className="center-buttons">
+          <button className="custom-button">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </button>
+          <button className="custom-button">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </button>
+          <button className="custom-button">
+            <Link to="/contact" className="nav-link">
+              Fitness
+            </Link>
+          </button>
+          <button className="custom-button">
+            <Link to="/contact" className="nav-link">
+              Health
+            </Link>
+          </button>
+          <button className="custom-button">
+            <Link to="/contact" className="nav-link">
+              Store
+            </Link>
+          </button>
+        </div>
+        <div className="right-buttons">
+          <button className="custom-button">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </button>
+          <button className="custom-button">
+            <Link to="/account" className="nav-link">
+              Account
+            </Link>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
