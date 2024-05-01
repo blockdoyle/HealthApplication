523ccac2e039d6e69e1752e24a0022a89a54fa8d
MohnishBhujun <mohnishbhujun@gmail.com>
2024-05-01 13:22:44 -0400

    Fixed login problem, Update User Model, Added Update and deleted User Resolvers

--- server/db/db.config.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const MONGODB = process.env.MONGODB || "mongodb://localhost:27017/healthApp";
-module.exports = {
-  MONGODB,
-};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,38 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    id: ID!
+    _id: ID!
     username: String!
     email: String!
+    sex: String
+    height: Float
+    weight: Float
+    age: Int
+    maximumCaloricIntake: Float
+    favouriteExercises: [String]
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+    sex: String
+    height: Float
+    weight: Float
+    age: Int
+    maximumCaloricIntake: Float
+    favouriteExercises: [String]
   }
 
   type AuthPayload {
     token: String!
     user: User!
+  }
+
+  type Query {
+    users: [User]
+    user(id: ID!): User
   }
 
   type Calorie {
@@ -44,23 +67,44 @@ const typeDefs = gql`
 
   type Query {
     me: User
-    dailyCalorieIntake: [Calorie]  
+    dailyCalorieIntake: [Calorie]
     allExercises: [Exercise]
     getExerciseById(id: ID!): Exercise
     allSupplements: [Supplement]
     getSupplementById(id: ID!): Supplement
     dailyWeightEntries: [Weight]
   }
-  
+
   type Mutation {
     login(email: String!, password: String!): AuthPayload
-    addUser(username: String!, email: String!, password: String!): AuthPayload
+    addUser(input: UserInput!): AuthPayload
+    updateUser(id: ID!, input: UserInput!): User
+    deleteUser(id: ID!): User
+
     addCalorieIntake(intake: Int!): Calorie
-    addExercise(name: String!, caloriesBurned: Int!, sets: Int!, reps: Int!, bodyPart: String!): Exercise
-    updateExercise(id: ID!, name: String, caloriesBurned: Int, sets: Int, reps: Int, bodyPart: String): Exercise
+    addExercise(
+      name: String!
+      caloriesBurned: Int!
+      sets: Int!
+      reps: Int!
+      bodyPart: String!
+    ): Exercise
+    updateExercise(
+      id: ID!
+      name: String
+      caloriesBurned: Int
+      sets: Int
+      reps: Int
+      bodyPart: String
+    ): Exercise
     deleteExercise(id: ID!): Exercise
     addSupplement(name: String!, description: String, price: Float!): Supplement
-    updateSupplement(id: ID!, name: String, description: String, price: Float): Supplement
+    updateSupplement(
+      id: ID!
+      name: String
+      description: String
+      price: Float
+    ): Supplement
     deleteSupplement(id: ID!): Supplement
     addWeightEntry(weight: Float!): Weight
   }
