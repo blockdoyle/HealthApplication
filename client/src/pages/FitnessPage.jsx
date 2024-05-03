d61e29d78210510b33367981dae1cf65b6897fd0
Joyce77777777 <145174906+Joyce77777777@users.noreply.github.com>
2024-05-03 00:25:29 -0700

    Update FitnessPage.jsx to use arm workout component

--- client/src/pages/FitnessPage.jsx
+++ client/src/pages/FitnessPage.jsx
@@ -1,41 +1,34 @@
-import { Menu, Button } from "antd";
-import "./FitnessPage.css";
-
-// Import fitness pages
+import React, { useState } from 'react';
+import { Menu } from 'antd';
+import ArmWorkoutComponent from '../components/ArmWorkout';
 
 export default function FitnessPage() {
+  const [currentPage, setCurrentPage] = useState('1');
+
   const menuItems = [
-    {
-      key: "1",
-      label: "Arms",
-    },
-    {
-      key: "2",
-      label: "Legs",
-    },
-    {
-      key: "3",
-      label: "Core",
-    },
-    {
-      key: "4",
-      label: "Chest",
-    },
-    {
-      key: "5",
-      label: "Glutes",
-    },
+    { key: "1", label: "Arms", component: <ArmWorkoutComponent /> },
+    { key: "2", label: "Legs", component: null },
+    { key: "3", label: "Core", component: null },
+    { key: "4", label: "Chest", component: null },
+    { key: "5", label: "Glutes", component: null },
   ];
+
   return (
     <>
       <div className="muscle-menu" style={{ width: 256 }}>
-        <Menu defaultSelectedKeys={["1"]} mode="inline">
-          {menuItems.map((item) => (
-            <Menu.Item key={item.key}>{item.label}</Menu.Item>
-          ))}
-        </Menu>
+        <Menu 
+          defaultSelectedKeys={['1']} 
+          mode="inline"
+          items={menuItems.map(item => ({
+            key: item.key,
+            label: item.label,
+            onClick: () => setCurrentPage(item.key)
+          }))}
+        />
       </div>
-      <div className="main">{/* Add fitness pages here */}</div>
+      <div className="main">
+        {menuItems.find(item => item.key === currentPage)?.component}
+      </div>
     </>
   );
 }
