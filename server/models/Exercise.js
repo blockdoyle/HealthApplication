9dcde6f211933e22d53b2854c4b66cec6ec9a340
Joyce77777777 <145174906+Joyce77777777@users.noreply.github.com>
2024-05-03 00:26:31 -0700

    Update Exercise.js to use set data as required by schema

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -4,7 +4,7 @@ const { Schema, model } = require("mongoose");
 const exerciseSchema = new Schema({
   name: { type: String, required: true },
   caloriesBurned: { type: Number, required: true },
-  // sets: { type: Number, required: true },
+  sets: { type: Number, required: true },
   reps: { type: Number, required: true },
   bodyPart: { type: String, required: true },
 });
