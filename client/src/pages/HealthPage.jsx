383f0ba43fc863694240cc4673525d04f6b8d2e7
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-05-04 21:33:02 -0400

    added handlers for input and submission field

--- client/src/pages/HealthPage.jsx
+++ client/src/pages/HealthPage.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react";
 import { Row, Col, Menu, Space, Input, Button } from "antd";
 import "./HealthPage.css";
+
 export default function HealthPage() {
-  const items = [
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    const response = fetch(
+      `https://api.calorieninjas.com/v1/nutrition?query=${input}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Api-Key": "j0Hq2gFfpven8IKukDhVUA==up1MPulZ53BOabJm",
+        },
+      }
+    );
+
+    response
+      .then((res) => {
+        const contentType = res.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+          return res.json();
+        } else {
+          throw new Error("Response was not JSON");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log();
+      });
+  };
+
+  const menu = [
     {
       key: "1",
-      label: "Food Log",
+      label: "My Health",
     },
     {
       key: "2",
+      label: "Food Log",
+    },
+    {
+      key: "3",
       label: "History",
     },
   ];
@@ -16,14 +57,19 @@ export default function HealthPage() {
       <div>
         <Row>
           <Col span={4}>
-            <Menu defaultSelectedKeys={["1"]} mode="inline" items={items} />
+            <Menu defaultSelectedKeys={["1"]} mode="inline" items={menu} />
           </Col>
           <Col span={1} />
           <Col span={18} className="health-main">
             <div className="search-bar">
               <Space.Compact style={{ width: "45%" }}>
-                <Input placeholder={"Enter what you ate."} />
-                <Button type="primary">Submit</Button>
+                <Input
+                  placeholder={"Enter what you ate."}
+                  onChange={(e) => handleInputChange(e)}
+                />
+                <Button type="primary" onClick={handleSearch}>
+                  Submit
+                </Button>
               </Space.Compact>
               <span style={{ fontStyle: "italic", marginBottom: "10px" }}>
                 This food tracker is powered by{" "}
