7b1727ce00240a166f8d231f1393a74b91f523ac
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-05-07 00:09:44 -0400

    changed soft_max_caloric_intake requirement to false

--- server/models/calorie.js
+++ server/models/calorie.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const calorieSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, default: Date.now },
   daily_calorie_intake: { type: Number, required: true },
   daily_calorie_expenditure: { type: Number, required: true },
@@ -9,15 +9,15 @@ const calorieSchema = new mongoose.Schema({
     type: Number,
     required: true,
     // Calculate net calorie balance as intake minus expenditure
-    default: function() {
+    default: function () {
       return this.daily_calorie_intake - this.daily_calorie_expenditure;
-    }
+    },
   },
   soft_max_caloric_intake: {
     type: Number,
     // This value should be taken from the user's document
-    required: true
-  }
+    required: false, // changed to false because it's not required
+  },
 });
 
-module.exports = mongoose.model('Calorie', calorieSchema);
+module.exports = mongoose.model("Calorie", calorieSchema);
